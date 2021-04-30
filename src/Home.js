0158c09c9b78bab0771430ab6b4d52451a386760
Seraj Alam <rajalam2372@gmail.com>
2021-04-30 20:15:23 +0530

    ss

--- src/Home.js
+++ src/Home.js
@@ -141,7 +141,7 @@ class Home extends Component {
                                     <td>{product.dueDate.split("T")[0]}</td>
                                     <td>{product.status} <span> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-pencil"></i></button></span>
 
-                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
